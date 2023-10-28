8a2b1748745623dda4237c76362e34325be26fc0
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-10-28 23:19:05 +0300

    2

--- routes/api/filters.js
+++ routes/api/filters.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getAllGlasses } = require("../../controllers/filters/getAllglasses");
 const { getIngredients } = require("../../controllers/filters/ingredients");
-const  getAllCategory = require("../../controllers/filters/categories")
+const  getAllCategory = require("../../controllers/filters/getAllCategories")
 const { authenticate } = require("../../middlewares/index.js");
 
 const router = express.Router();
