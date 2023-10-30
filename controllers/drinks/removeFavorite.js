4fc1fa407acc2dd9fa9c52ef47a1501353f1247b
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-10-30 14:24:21 +0200

    fix

--- controllers/drinks/removeFavorite.js
+++ controllers/drinks/removeFavorite.js
@@ -1,18 +1,24 @@
 const { Drink } = require("../../models/cocktails");
 const { ctrlWrapper, HttpError } = require("../../utils");
 
-const removeFavorite = ctrlWrapper(async (req, res, next) => {
-  const { drinkId } = req.params;
-  const { id: userId } = req.user;
-  const result = await Drink.findByIdAndUpdate(drinkId, {
-    $pull: { users: userId },
-  });
-  if (!result) {
-    throw HttpError(404, "Not found");
+const removeFavorite = ctrlWrapper(
+  async (req, res, next) => {
+    const { drinkId } = req.params;
+    const { id: userId } = req.user;
+    const result = await Drink.findByIdAndUpdate(drinkId, {
+      $pull: { users: userId },
+    });
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    if (!result.users.includes(userId)) {
+      res.status(409).json({
+        message: "This drink is not in favorites",
+      });
+    } else {
+      res.status(200).json({ message: "Drink deleted" });
+    }
   }
-  res
-    .status(200)
-    .json({ message: "Drink deleted" });
-});
+);
 
 module.exports = removeFavorite;
