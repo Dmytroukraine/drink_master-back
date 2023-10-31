8ce9e58db84682d926057a56b973e853092df0ae
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-11-01 00:41:05 +0200

    Update getPopular.js

--- controllers/drinks/getPopular.js
+++ controllers/drinks/getPopular.js
@@ -10,38 +10,9 @@ const getPopular = ctrlWrapper(async (req, res, next) => {
 
   if (age < 18) {
 
-// const pipeline = [
-// 		{
-// 			'$match': {
-// 				"usersId": { $exists: true }
-// 			}
-// 		},
-// 		{
-// 			$project: {
-// 				"drink": 1,
-// 				"instructions": 1,
-// 				"drinkThumb": 1,
-// 				"usersId": 1,
-// 				"users_count": { $size: "$usersId" }
-// 			}
-// 		},
-// 		{ $sort: { "users_count": -1 } }, {
-// 			'$limit': 4
-// 		}
-// 	]
-
-// 	const result = await Recipe.aggregate(pipeline);
-
-
 
     const result = await Drink.aggregate([
-      // {
-      //   $cond: {
-      //     if: { users: { $exists: true } },
-      //     then:
-      //   }
-      // },
-      // { users: { $exists: true } },
+
         { $match: { users: { $exists: true }, alcoholic: "Non alcoholic" } },
         { $sort: { users: -1 } },
         { $limit: 4 },
