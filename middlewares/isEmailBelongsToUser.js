09c8334fbb70407503f7e10b46b56330e9363f3f
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-11-05 22:57:34 +0100

    fixed middleware

--- middlewares/isEmailBelongsToUser.js
+++ middlewares/isEmailBelongsToUser.js
@@ -3,16 +3,15 @@ const { HttpError } = require("../utils");
 
 const isEmailBelongsToUser = async (req, res, next) => {
   const email = req.body.email;
-  console.log(req.user);
 
   const user = await User.findOne({ email });
   if (!user) {
     return next(HttpError(404, "Not found"));
   }
-  console.log(user);
-  if (email !== user.email) {
-    throw HttpError(404, "Please, enter your email");
+  if (email !== req.user.email) {
+    return next(HttpError(404, "Please, enter your email"));
   }
+
   next();
 };
 
